3789337a325fada5306693cbb460e713583453da
LordEra <marvinezenwekwe@gmail.com>
2022-01-01 14:14:50 +0100

    First Next project

---
+++ comps/Navbar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import Image from 'next/image';
+const Navbar = () => {
+  return (  
+    <nav>
+        <div className="logo">
+          <Image width={150} height={77} src="/logo.png" />
+        </div>
+        <Link href="/"><a> Home </a></Link>
+        <Link href="/about"><a> About </a></Link>
+        <Link href="/contact"><a> Contact Us </a></Link>
+    </nav>
+  );
+}
+ 
+export default Navbar;
---
+++ pages/404.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000)
+  }, [])
+
+  return (  
+    <div className="not-found">
+      <h1> Oops...</h1>
+      <h2> Page Not Found </h2>
+      <p> Back to <Link href="/"><a>Homepage </a></Link></p>
+    </div>
+  );
+}
+ 
+export default NotFound;
